b38ce12dc4248e8eda62fbe65ed0f6e410606e7f
thalismind <admin@thalis.dev>
2025-01-12 14:27:20 -0600

    allow extra args to comfy

--- scripts/run_comfyui.sh
+++ scripts/run_comfyui.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+EXTRA_ARGS="${@:1}"
+echo "Running comfyui with extra args: $EXTRA_ARGS"
+
 cd /opt/comfyui
 source venv/bin/activate
-python main.py --cpu --listen 0.0.0.0 --output-directory /var/lib/comfyui/output
+python main.py $EXTRA_ARGS --listen 0.0.0.0 --output-directory /var/lib/comfyui/output
